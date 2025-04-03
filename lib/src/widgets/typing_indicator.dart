e77316041850deb51a4f545b1656472a538d505e
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-04-03 19:16:59 +0500

    Add TypingIndicator widget for chat applications
    This commit introduces a new TypingIndicator widget that visually represents when a user is typing in a chat interface. The widget features customizable dot color and size, along with an animation that enhances the user experience by indicating activity in the chat. This addition complements the existing ChatUI and ChatBubble widgets, further improving the chat interface.

---
+++ lib/src/widgets/typing_indicator.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class TypingIndicator extends StatefulWidget {
+  final Color dotColor;
+  final double dotSize;
+  final Duration animationDuration;
+
+  const TypingIndicator({
+    Key? key,
+    this.dotColor = Colors.grey,
+    this.dotSize = 8.0,
+    this.animationDuration = const Duration(milliseconds: 1500),
+  }) : super(key: key);
+
+  @override
+  State<TypingIndicator> createState() => _TypingIndicatorState();
+}
+
+class _TypingIndicatorState extends State<TypingIndicator>
+    with TickerProviderStateMixin {
+  late List<AnimationController> _animationControllers;
+  late List<Animation<double>> _animations;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeAnimations();
+  }
+
+  void _initializeAnimations() {
+    _animationControllers = List.generate(
+      3,
+      (index) => AnimationController(
+        duration: widget.animationDuration,
+        vsync: this,
+      ),
+    );
+
+    _animations = _animationControllers.map((controller) {
+      return Tween<double>(begin: 0.0, end: 1.0).animate(
+        CurvedAnimation(
+          parent: controller,
+          curve: Curves.easeInOut,
+        ),
+      );
+    }).toList();
+
+    for (var i = 0; i < 3; i++) {
+      Future.delayed(Duration(milliseconds: i * 300), () {
+        if (mounted) {
+          _animationControllers[i].repeat(reverse: true);
+        }
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    for (var controller in _animationControllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: List.generate(3, (index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 2),
+            child: AnimatedBuilder(
+              animation: _animations[index],
+              builder: (context, child) {
+                return Transform.translate(
+                  offset: Offset(0, -4 * _animations[index].value),
+                  child: Container(
+                    width: widget.dotSize,
+                    height: widget.dotSize,
+                    decoration: BoxDecoration(
+                      color: widget.dotColor,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        }),
+      ),
+    );
+  }
+} 
