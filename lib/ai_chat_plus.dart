c25e53ee913d2267be4514dd0281ae1017adb9ec
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-04-03 11:00:23 +0500

    Remove unused Calculator class from ai_chat_plus.dart to streamline code and improve maintainability.

--- lib/ai_chat_plus.dart
+++ lib/ai_chat_plus.dart
@@ -18,8 +18,3 @@ class AIChat {
   // Implementation will be added in future updates
 }
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
